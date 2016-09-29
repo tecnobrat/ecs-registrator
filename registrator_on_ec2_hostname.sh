84ed8e92b9dab5d2e92431bc8f4887cccfb4e42d
Brian Stolz <brian@tecnobrat.com>
2016-09-29 12:20:59 -0700

    Update registrator_on_ec2_hostname.sh

--- registrator_on_ec2_hostname.sh
+++ registrator_on_ec2_hostname.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LOCAL_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-echo Running: registrator -ip $LOCAL_IP $@ consul://$LOCAL_IP:8500
-exec registrator -ip $LOCAL_IP "$@" consul://$LOCAL_IP:8500
+echo "Running: registrator -ip $LOCAL_IP $@"
+exec registrator -ip $LOCAL_IP "$@"
